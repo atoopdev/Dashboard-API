a0a8b95d83fd8ff6ea0c2657564ffe5c37fa158e
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-04-18 14:09:57 -0700

    apply to body directly

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 getBackgroundImage()
 
-// async function getBackgroundImage(){
-//     let response = await fetch(`https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature`, {method: "GET"})
-//     let data = await response.json()
-//     console.log("Image data: ", data)
-//     let backgroundEL = document.getElementById("bg-img")
 
-//     backgroundEL.innerHTML = `<img src = "${data.urls[0]}" alt = "${data.alt_description}" />`
-    
-// }
 
 function getBackgroundImage(){
     fetch(`https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature`, {method: "GET"})
@@ -16,9 +8,7 @@ function getBackgroundImage(){
     .then(data=>{
         console.log(data)
         
-        let backgroundEL = document.getElementById("bg-img")
-        backgroundEL.style.backgroundImage = `url('${data.urls.regular}')`
+        document.body.style.backgroundImage = `url('${data.urls.full}')`
 
-        // backgroundEL.innerHTML = `<img src= "${data.urls.regular}" alt="${data.alt_description}" />`
     })
 }
