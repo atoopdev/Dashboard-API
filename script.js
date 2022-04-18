27e9c38558557ecdc19ebdc022c185c256ba42b2
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-04-18 16:10:32 -0700

    Make photo credit clickable link

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ function getBackgroundImage(){
         console.log(data)
         
         document.body.style.backgroundImage = `url('${data.urls.full}')`
-        document.getElementById("photo-credit").innerText = `Photo credit: ${data.user.name}`
+        document.getElementById("photo-credit").innerHTML = `<a href="${data.links.html}">Photo credit: ${data.user.name}</a>`
+    })
+    .catch(err =>{
+        console.error(err)
+        // handle error here
+        // add default background image
+        document.body.style.backgroundImage = `url('./default.jpg')`
+        document.getElementById("photo-credit").innerHTML = `<a href="https://unsplash.com/photos/x1w_Q78xNEY">Photo credit: Grant Ritchie</a>`
     })
 }
